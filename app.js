a0760f81866d41b9a89f8a0b15af612737d92189
Axlejandro <aalejandro.armesto@gmail.com>
2022-07-06 15:08:43 -0300

    agregando formularios

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
-const express = require ('express');
+const express = require('express');
 const path = require('path');
 
 const app = express();
 
-const publicPath = path.resolve(__dirname, './public');
-app.use(express.static(publicPath));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 
-app.listen(3000, () =>{
-    console.log ("Servidor corriendo en el puerto 3000")
+const pathPublic = path.resolve(__dirname, './public');
+app.use(express.static(pathPublic));
+
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/home.html');
 });
 
-app.get('/', (req, res)=>{
-   res.sendFile(path.resolve(__dirname, './views/home.html'))
-})
+app.post('/', (req, res) => {
+    res.redirect('/');
+});
+
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
+
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
+
+app.listen(3000, ()=>{
+    console.log('Servidor funcionando en: http://localhost:3000');
+});
